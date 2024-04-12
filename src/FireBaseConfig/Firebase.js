7726498e39392ec7520e377cadd3072d7633edbd
Md Ismail <mdismaeelkhan345@gmail.com>
2024-04-12 22:03:18 +0530

    Updation

--- src/FireBaseConfig/Firebase.js
+++ src/FireBaseConfig/Firebase.js
@@ -2,13 +2,13 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCSQJixJu8MMg2linzJOcpcFpIpdR6UXNM",
-    authDomain: "drive-react-4255c.firebaseapp.com",
-    projectId: "drive-react-4255c",
-    storageBucket: "drive-react-4255c.appspot.com",
-    messagingSenderId: "926952289144",
-    appId: "1:926952289144:web:1e64c8ddb69a4ea375ce8d",
-    measurementId: "G-WW4JTKZLJ6"
+    apiKey: "AIzaSyDyQXGbugR1vAKKQjpFXvICk__xVUu6acw",
+    authDomain: "drive-38585.firebaseapp.com",
+    projectId: "drive-38585",
+    storageBucket: "drive-38585.appspot.com",
+    messagingSenderId: "909215945697",
+    appId: "1:909215945697:web:1bbf823a759fa14689f8a0",
+    measurementId: "G-QSWXGWX4ZX"
 };
 
 
